d103c7dec169ee3fc71b96d3ac27bd5b622e866f
Ivanaricci <ivana.ricci26@gmail.com>
2025-05-05 17:54:34 +0200

    [FIX] Fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,21 @@ const Header = () => {
             <figure>
                 <img src="dc-logo.png" alt="Dc Comics" />
             </figure>
+            <ul>
+                <li><a href="#">characters</a></li>
+                <li><a href="#">comics</a></li>
+                <li><a href="#">movies</a></li>
+                <li><a href="#"></a>tv</li>
+                <li><a href="#"></a>games</li>
+                <li><a href="#"></a>collectibles</li>
+                <li><a href="#"></a>videos</li>
+                <li><a href="#"></a>fans</li>
+                <li><a href="#"></a>news</li>
+                <li><a href="#"></a>shop</li>
+            </ul>
         </header>
     )
 }
 
 export default Header
+
