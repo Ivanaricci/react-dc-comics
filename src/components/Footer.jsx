24fef725bb45866e423b085be0569f1013a0c2b3
Ivanaricci <ivana.ricci26@gmail.com>
2025-05-05 19:26:02 +0200

    [UPD] Add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,32 +3,58 @@
 const Footer = () => {
     return (
         <footer>
-            <div className="footer.top">
-                <div>
-                    <div>
-                        <h3>dc comics</h3>
+            <div className="footer-top">
+                <div className="footer-left">
+                    <div className="text-footer">
+                        <div>
+                            <h3>dc comics</h3>
+                            <ul>
+                                <li>Chatacters</li>
+                                <li>Chatacters</li>
+                                <li>Chatacters</li>
+                                <li>Chatacters</li>
+                                <li>Chatacters</li>
+                                <li>Chatacters</li>
+                                <li>Chatacters</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h3>shop</h3>
+                            <ul>
+                                <li>Chatacters</li>
+                                <li>Chatacters</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="text-footer">
+                        <h3>dc</h3>
                         <ul>
-                            <li></li>
+                            <li>Chatacters</li>
+                            <li>Chatacters</li>
+                            <li>Chatacters</li>
+                            <li>Chatacters</li>
+                            <li>Chatacters</li>
+                            <li>Chatacters</li>
+                            <li>Chatacters</li>
+                            <li>Chatacters</li>
+                            <li>Chatacters</li>
+                            <li>Chatacters</li>
+                            <li>Chatacters</li>
                         </ul>
                     </div>
-                    <div>
-                        <h3>dc comics</h3>
+                    <div className="text-footer">
+                        <h3>sites</h3>
                         <ul>
-                            <li></li>
+                            <li>Chatacters</li>
+                            <li>Chatacters</li>
+                            <li>Chatacters</li>
+                            <li>Chatacters</li>
+                            <li>Chatacters</li>
                         </ul>
                     </div>
                 </div>
-                <div>
-                    <h3>dc</h3>
-                    <ul>
-                        <li></li>
-                    </ul>
-                </div>
-                <div>
-                    <h3>dc</h3>
-                    <ul>
-                        <li></li>
-                    </ul>
+                <div className="footer-right">
+                    <img className="img-footer" src="../src/assets/img/dc-logo-bg.png" alt="" />
                 </div>
             </div>
             <div className="footer-bottom"></div>
