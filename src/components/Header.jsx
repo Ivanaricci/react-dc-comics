87b95f0c8fc1dab7fca02648394327c7ef9af8fe
Ivanaricci <ivana.ricci26@gmail.com>
2025-05-05 17:58:13 +0200

    [FIX] Fix menu header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,13 +9,13 @@ const Header = () => {
                 <li><a href="#">characters</a></li>
                 <li><a href="#">comics</a></li>
                 <li><a href="#">movies</a></li>
-                <li><a href="#"></a>tv</li>
-                <li><a href="#"></a>games</li>
-                <li><a href="#"></a>collectibles</li>
-                <li><a href="#"></a>videos</li>
-                <li><a href="#"></a>fans</li>
-                <li><a href="#"></a>news</li>
-                <li><a href="#"></a>shop</li>
+                <li><a href="#">tv</a></li>
+                <li><a href="#">games</a></li>
+                <li><a href="#">collectibles</a></li>
+                <li><a href="#">videos</a></li>
+                <li><a href="#">fans</a></li>
+                <li><a href="#">news</a></li>
+                <li><a href="#">shop</a></li>
             </ul>
         </header>
     )
