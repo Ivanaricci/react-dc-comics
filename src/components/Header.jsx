7bdc18470b5b4f2790a574ff043e35aec6f6786e
Ivanaricci <ivana.ricci26@gmail.com>
2025-05-05 17:35:25 +0200

    [UPD] Add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+    return (
+        <header>
+            <figure>
+                <img src="dc-logo.png" alt="Dc Comics" />
+            </figure>
+        </header>
+    )
+}
+
+export default Header
