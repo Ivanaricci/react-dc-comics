de543e61b5e098d13dd503ce71fee730fdd52cb9
Ivanaricci <ivana.ricci26@gmail.com>
2025-05-06 17:08:45 +0200

    [UPD] Add jumbotron

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,25 @@
 
 const Header = () => {
     return (
-        <header className="container">
-            <figure>
-                <img src="../src/assets/img/dc-logo.png" alt="Dc Comics" />
-            </figure>
-            <ul>
-                <li><a href="#">characters</a></li>
-                <li><a href="#">comics</a></li>
-                <li><a href="#">movies</a></li>
-                <li><a href="#">tv</a></li>
-                <li><a href="#">games</a></li>
-                <li><a href="#">collectibles</a></li>
-                <li><a href="#">videos</a></li>
-                <li><a href="#">fans</a></li>
-                <li><a href="#">news</a></li>
-                <li><a href="#">shop</a></li>
-            </ul>
+        <header>
+            <div className="container header-top">
+                <figure>
+                    <img src="../src/assets/img/dc-logo.png" alt="Dc Comics" />
+                </figure>
+                <ul>
+                    <li><a href="#">characters</a></li>
+                    <li><a href="#">comics</a></li>
+                    <li><a href="#">movies</a></li>
+                    <li><a href="#">tv</a></li>
+                    <li><a href="#">games</a></li>
+                    <li><a href="#">collectibles</a></li>
+                    <li><a href="#">videos</a></li>
+                    <li><a href="#">fans</a></li>
+                    <li><a href="#">news</a></li>
+                    <li><a href="#">shop</a></li>
+                </ul>
+            </div>
+            <div className="jumbotron"></div>
         </header>
     )
 }
