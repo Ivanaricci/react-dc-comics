a1160c607f95c9d1f921c6807dde03d3f0f6a73d
Ivanaricci <ivana.ricci26@gmail.com>
2025-05-07 16:39:48 +0200

    [UPD] Done Bonus

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,69 @@
 
 const Header = () => {
+
+    const links = [
+        {
+            id: 1,
+            href: "#",
+            label: "Characters",
+            current: false,
+        },
+        {
+            id: 2,
+            href: "#",
+            label: "Comics",
+            current: true,
+        },
+        {
+            id: 3,
+            href: "#",
+            label: "Movies",
+            current: false,
+        },
+        {
+            id: 4,
+            href: "#",
+            label: "TV",
+            current: false,
+        },
+        {
+            id: 4,
+            href: "#",
+            label: "Games",
+            current: false,
+        },
+        {
+            id: 4,
+            href: "#",
+            label: "Collectibles",
+            current: false,
+        },
+        {
+            id: 4,
+            href: "#",
+            label: "Videos",
+            current: false,
+        },
+        {
+            id: 4,
+            href: "#",
+            label: "Fans",
+            current: false,
+        },
+        {
+            id: 4,
+            href: "#",
+            label: "News",
+            current: false,
+        },
+        {
+            id: 4,
+            href: "#",
+            label: "Shop",
+            current: false,
+        }
+    ]
+
     return (
         <header>
             <div className="container header-top">
@@ -7,7 +71,7 @@ const Header = () => {
                     <img src="../src/assets/img/dc-logo.png" alt="Dc Comics" />
                 </figure>
                 <ul>
-                    <li><a href="#">characters</a></li>
+                    {/* <li><a href="#">characters</a></li>
                     <li><a href="#">comics</a></li>
                     <li><a href="#">movies</a></li>
                     <li><a href="#">tv</a></li>
@@ -16,7 +80,13 @@ const Header = () => {
                     <li><a href="#">videos</a></li>
                     <li><a href="#">fans</a></li>
                     <li><a href="#">news</a></li>
-                    <li><a href="#">shop</a></li>
+                    <li><a href="#">shop</a></li> */}
+
+                    {links.map((link) => (
+                        <li key={`link-${link.id}`}>
+                            <a href={link.href}>{link.label}</a>
+                        </li>
+                    ))}
                 </ul>
             </div>
             <div className="jumbotron"></div>
